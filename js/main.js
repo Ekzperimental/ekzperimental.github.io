6dc93eb76eaae360155162674ece4c812ca7869b
Ekzperimental <k.ekzarov@swkz.org>
2026-07-15 09:21:21 +0300

    Release Version 2.6

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { initNavbar } from "./navbar.js?v=20260707-26";
 import { initHero } from "./hero.js?v=20260707-26";
 import { initRevealAnimations } from "./animations.js?v=20260707-26";
-import { initWork } from "./work.js?v=20260707-26";
+import { initWork } from "./work.js?v=20260715-01";
 import { initContactLinks } from "./contact.js?v=20260707-26";
 
 const boot = () => {
